e66bf0d1f8d64c403b6b56dc9b90e195dc21f55b
LuanBertozzi <LuanBertozziS2@hotmail.com>
2024-06-23 20:34:15 -0400

    updated code, major changes, complete refactoring based on book code base

--- startWeb.js
+++ startWeb.js
@@ -1,39 +1,34 @@
-const http = require('http');
-const fs = require('fs').promises;
+const hhtp = require('http');
+const fs = require('fs');
+const url = require('url');
 const path = require('path');
 
-async function readArchive(archivePath) {
-    try {
-        const data = await fs.readFile(archivePath, 'utf8');
-        return data;
-    } catch (error) {
-        throw new Error(`Erro ao ler o arquivo -> ${error}`);
-    }
-}
-
-const startServer = http.createServer(async function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    
-    if (request.url == '/' || request.url == '/artigos') {
-        try {
-            const archivePath = path.join(__dirname, 'Pages', 'artigos.html');
-            const archiveContent = await readArchive(archivePath);
-            response.write(archiveContent);
-        } catch (error) {
-            console.error(error);
-            response.writeHead(500, {'Content-Type': 'text/plain'});
-            response.write('Erro interno ao processar a requisição.');
+// Função de Gerenciar rotas
+const rotear = function(pathname){
+    if (pathname && pathname !== '/'){
+        const arquivo = path.join(__dirname, `${pathname}.html`)
+        const existe = fs.existsSync(arquivo);
+        if (existe) {
+            return arquivo;
         }
-    } else if (request.url == '/contatos') {
-        response.write('<h1>Contatos Page</h1>');
-    } else {
-        response.writeHead(404, {'Content-Type': 'text/plain'});
-        response.write('Página não encontrada.');
+        return path.join(__dirname, 'erro.html');
     }
-    
-    response.end();
+    return path.join(__dirname, 'artigos.html');
+};
+
+// Iniciando o servidor 
+const server = http.createServer((request, response) => {
+    // Obtendo o pathname
+    const pathname = url.parse(request.url).pathname;
+    // Processando o roteamento do pathname 
+    const pagina = rotear(pathname);
+    // Renderizando
+    fs.readFile(pagina, (err, html) => {
+        response.writeHeader(200, {'Content-type' : 'text/html'});
+        response.end(html);
+    });
 });
 
-startServer.listen(3000, function() {
-    console.log("#### Server ON ####");
-});
+server.listen(3000, () => {
+    console.log("### Server ON ###");
+})
